bf82353eec99a87aaea69d9a28611a859ee6f1dc
WasanFahad44 <wasan107@icloud.com>
2024-10-21 14:51:03 +0300

    Initial Commit

---
+++ helloo world pro/helloo_world_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloo_world_proApp.swift
+//  helloo world pro
+//
+//  Created by wasan on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct helloo_world_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
